9768fcbd0efaded86f13ec9ced15ad00ccd0e63a
gbaudry303 <gbaudry303@gmail.com>
2015-08-13 17:00:09 -0700

    escaping fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 
 module.exports = {
   generateHtml: function(filename, next) {
-    exec('unoconv -f html ' + filename, function (error, stdout, stderr) {
+    exec('unoconv -f html "' + filename +'"', function (error, stdout, stderr) {
       if (error) return next(error);
       return next(null, 'HTML of ' + filename + ' has been successfully generated.' );
     });
